21627df66a15b7396b60acd7f9632580e0eb4fb2
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-05 11:01:16 -0700

    expanded on createTodayCard function

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,58 @@ let key = 'cc8fe01df44bc4c62528873d53642314';
 let cityInput = document.querySelector('#cityInput');
 let searchBtn = document.querySelector('#searchBtn');
 let todayCardContainer = document.querySelector('#today-card-container');
+let currWeather;
 
-function createTodayCard(data) {
-    let cityName = data.name;
-    let weather = data[0].main;
-    let weatherDesc = data[0].description;
-    let temperature = data.main.feels_like.toFixed(1);
-    let humidity = data.main.humidity;
-    let wind = data.wind.speed;
+function createTodayCard(name, data) {
+    let cityName = name;
+    let weather = data.current.weather[0].main;
+    currWeather = weather;
+    let weatherDesc = data.current.weather[0].description;
+    let temperature = data.current.feels_like.toFixed(1);
+    let humidity = data.current.humidity;
+    let wind = data.current.wind_speed;
+
+    //Weather icon
+    let iconCode = data.current.weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+    //create the today card
+    let todayCardDiv = document.createElement('div');
+    todayCardDiv.classList.add('card');
+    todayCardDiv.setAttribute('id', 'today-card');
+
+    //create the header for the today card
+    let todayHeaderDiv = document.createElement('div');
+    todayHeaderDiv.classList.add('card-header');
+
+    //create the title for the header
+    let todayTitle = document.createElement('h2');
+    todayTitle.classList.add('card-title');
+    todayTitle.textContent = cityName;
+
+    //Create the icon image
+    let todayIcon = document.createElement('img');
+    todayIcon.setAttribute('id', 'today-icon');
+    todayIcon.setAttribute('src', iconUrl);
+
+    //append the title and image to the header
+    todayHeaderDiv.appendChild(todayTitle);
+    todayHeaderDiv.appendChild(todayIcon);
+
+    //append the header to the main card div
+    todayCardDiv.appendChild(todayHeaderDiv);
+
+    //Append the today card to the container div
+    todayCardContainer.appendChild(todayCardDiv);
 }
 
-function getCityData(lat, long) {
-    let url = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+function getCityData(name, lat, long) {
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly,daily&appid=${key}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => createTodayCard(name, data))
+        .catch(err => console.log(err));
 }
 
 //Function to retrieve geolocation of input city.
@@ -28,7 +68,7 @@ function getGeolocation(e) {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            getCityData(data[0].lat, data[0].lon);
+            getCityData(data[0].name, data[0].lat, data[0].lon);
         })
         .catch(err => console.log(err));
 }
